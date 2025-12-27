25c2a49110ca027b982fcf069016608549fa4d9a
sharanabasavas546 <sharanabasavas546@gmail.com>
2025-12-27 19:55:51 +0530

    Update product_sales.py

--- product_sales.py
+++ product_sales.py
@@ -3,7 +3,10 @@ from datetime import datetime
 # global sales record
 sales = []
 
-def process_sale(product, quantity):
+def process_sale(
+    product={"id": 0, "name": "Unknown", "price": 0, "stock": 0},
+    quantity=0
+):
     if quantity <= product["stock"]:
         total = product["price"] * quantity
         product["stock"] -= quantity
